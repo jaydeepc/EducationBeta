a56ee434f6f0a85c7fe1e449a7ac04d71da3a05c
Jaydeep <jaydeepc@thoughtworks.com>
2012-02-28 04:01:02 -0800

    Adding repository to Git

---
+++ EducationBeta/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EducationBeta::Application.config.secret_token = '1624e8a3e8bf56f372cdf611b8c1eb408bddf8d59d4bf437d322df2db3b7d972b00a942a5e2e3122036672726ca5cd91121722874137b2020aba6e8c574d0ff6'
