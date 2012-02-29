e5dbadb4798db3e7e384cd59fbf1dde5a8bcbd23
Sumanth <slicehost@thoughtworks.com>
2012-02-29 02:30:46 -0600

    Sumanth Added a controller and view for a welcome page

--- EducationBeta/config/routes.rb
+++ EducationBeta/config/routes.rb
@@ -1,4 +1,6 @@
 EducationBeta::Application.routes.draw do
+  get "welcome/show"
+
   get "sessions/new"
 
   get "users/new"
@@ -6,7 +8,7 @@ EducationBeta::Application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  root :to => "welcome#show"
   resources :users
   resources :sessions
 
