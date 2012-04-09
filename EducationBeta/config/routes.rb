f32b5c00c2e2e3baf3b9df00af64efe967ab097d
Jaydeep <jaydeepc@thoughtworks.com>
2012-04-09 12:44:36 -0700

    Jaydeep: Added UI for Questions model.

--- EducationBeta/config/routes.rb
+++ EducationBeta/config/routes.rb
@@ -9,6 +9,7 @@ EducationBeta::Application.routes.draw do
     resources :questions
   end 
   resources :sessions
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
