2a71cfdd643a70fe35235b3b49c215ca30be5acd
Sumanth <slicehost@thoughtworks.com>
2012-04-11 21:16:46 +0530

    Sumanth Added functionality to update a question asked

--- EducationBeta/app/controllers/questions_controller.rb
+++ EducationBeta/app/controllers/questions_controller.rb
@@ -27,7 +27,19 @@ class QuestionsController < ApplicationController
   end
 
   def index
-    @questions = Tutor.find(current_user.id).questions if current_user.is_tutor?
-    @questions = Student.find(current_user.id).questions unless current_user.is_tutor?
+    @questions = current_user.questions
+  end
+
+  def update
+   @question = Question.find(params[:id]) 
+   question_params = Hash.new
+   question_params[:tutor_id] = Tutor.find_by_email(params[:tutor]).id
+   question_params[:subject_id] = Subject.find_by_subject(params[:subject]).id
+   question_params[:description] = params[:question][:description]
+   if @question.update_attributes(question_params)
+     redirect_to user_questions_path
+   else
+     render "new"
+   end  
   end
 end
