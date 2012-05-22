a2b3762e125852e5c544bf5250b9900858bbe1e8
Jaydeep <jaydeepc@thoughtworks.com>
2012-05-22 08:43:59 +0530

    Added the UI for the automated email.<Jaydeep>

--- EducationBeta/app/mailers/user_mailer.rb
+++ EducationBeta/app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
   default :from => "sumanth@educationbeta.com"
-
+  
   def welcome_email(user)
     @user = user
     @url = "http://#{APP_CONFIG['domain']}:#{APP_CONFIG['port']}/validate/#{user.validation_uuid}"
--- EducationBeta/config/application.rb
+++ EducationBeta/config/application.rb
@@ -55,6 +55,12 @@ module EducationBeta
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.action_mailer.asset_host = 'http://localhost:3000'
+    config.action_mailer.default_url_options = {
+        :host => 'localhost:3000',
+        :only_path => false
+    }
 
   end
 end
