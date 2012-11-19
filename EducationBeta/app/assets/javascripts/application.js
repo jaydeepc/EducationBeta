5f797fd6eef651ea5640995e232f6b5dd4a459e9
Jaydeep <Admin@Admins-MacBook-Pro-3.local>
2012-11-19 13:17:13 +0530

    Added popup for validation and forgot password.<Jaydeep>

--- EducationBeta/app/assets/javascripts/application.js
+++ EducationBeta/app/assets/javascripts/application.js
@@ -1,15 +1,2 @@
 //= require jquery
 //= require jquery_ujs
-
-$(function(){
-    var $modal = $('#modal'),
-    $modal_close = $modal.find('.close'),
-    $modal_container = $('#modal-container');
-
-    // Hide close button click
-    $('.close', '#modal').live('click', function(){
-      $modal_container.hide();
-      $modal.css('width', '600px').hide();
-      return false;
-     });
-});
